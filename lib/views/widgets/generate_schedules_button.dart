fd5669a3c33155fb2e64050059217a6073c2d043
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-06-18 19:09:19 -0400

    Widget to manage generateButton

---
+++ lib/views/widgets/generate_schedules_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../constants/constants.dart';
+import '../../providers/providers.dart';
+import '../../constants/en_strings.dart';
+
+class GenerateSchedulesButton extends ConsumerWidget {
+  const GenerateSchedulesButton({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final Function? onPressed;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(generateStateProvider);
+
+    switch (state) {
+      case GenerateButtonState.disabled:
+        return _buildDisabledButton();
+      case GenerateButtonState.ready:
+        return _buildReadyButton(onPressed);
+      case GenerateButtonState.generating:
+        return _buildGeneratingButton();
+    }
+  }
+
+  Widget _buildReadyButton(onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: const ButtonStyle(),
+      child: const Text(courseListViewGenerateButton),
+    );
+  }
+
+  Widget _buildDisabledButton() {
+    return const ElevatedButton(
+      onPressed: null, // Disabled button, do nothing on press
+      child: Text(courseListViewGenerateButton),
+    );
+  }
+
+  Widget _buildGeneratingButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(primary: Colors.red),
+      child: Text(courseListViewGenerateButtonGenerating),
+    );
+  }
+}
