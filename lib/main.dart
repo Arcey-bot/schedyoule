a53a9a9c828ff813ef83ae16af21aebd98180b7e
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-07-10 02:18:39 -0400

    Add TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:schedyoule/views/home_view.dart';
 
 // TODO: Tips screen or something idk
+// TODO: When changing focus to time button from typing name, name is not saved
+// TODO: Courses should not reorder when modified
+// TODO: Courses updating incorrectly when a different course is modified
+// TODO: Add deadspace at bottom of course screen so FAB doesn't obscure time selector
+// TODO: Perhaps show card times in 24hr format
+// TODO: Opened/Closed schedules do not maintain state when offscreen
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
