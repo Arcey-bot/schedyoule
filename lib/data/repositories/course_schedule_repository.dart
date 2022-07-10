1e3b591b680cec523e4d0661493fe1ff58003e37
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-07-10 00:56:57 -0400

    Update

--- lib/data/repositories/course_schedule_repository.dart
+++ lib/data/repositories/course_schedule_repository.dart
@@ -53,33 +53,33 @@ class CourseScheduleRepository {
   /// Generate a number of possible schedules given a list of courses
   ///
   /// Will attempt to maximize amount of courses taken in a schedule.
-  Future<List<Schedule>> generateSchedules() async {
-    courses.sort();
+  // Future<List<Schedule>> generateSchedules() async {
+  //   courses.sort();
 
-    final List<Schedule> possibleSchedules = [
-      for (Course c in coursesAtOrBefore(latest!)) Schedule()..addCourse(c)
-    ];
+  //   final List<Schedule> possibleSchedules = [
+  //     for (Course c in coursesAtOrBefore(latest!)) Schedule()..addCourse(c)
+  //   ];
 
-    for (final Course course in courses) {
-      for (final Schedule schedule in possibleSchedules) {
-        if (schedule.canAddCourse(course)) {
-          schedule.addCourse(course);
-        } else {
-          final Schedule newSchedule = schedule.deepcopy();
-          Course? conflictCourse = newSchedule.conflictingCourse(course);
+  //   for (final Course course in courses) {
+  //     for (final Schedule schedule in possibleSchedules) {
+  //       if (schedule.canAddCourse(course)) {
+  //         schedule.addCourse(course);
+  //       } else {
+  //         final Schedule newSchedule = schedule.deepcopy();
+  //         Course? conflictCourse = newSchedule.conflictingCourse(course);
 
-          // Remove courses until there are no further conflicts
-          while (conflictCourse != null) {
-            newSchedule.removeCourse(conflictCourse);
-            conflictCourse = newSchedule.conflictingCourse(course);
-          }
-          newSchedule.addCourse(course);
-        }
-      }
-    }
+  //         // Remove courses until there are no further conflicts
+  //         while (conflictCourse != null) {
+  //           newSchedule.removeCourse(conflictCourse);
+  //           conflictCourse = newSchedule.conflictingCourse(course);
+  //         }
+  //         newSchedule.addCourse(course);
+  //       }
+  //     }
+  //   }
 
-    return possibleSchedules;
-  }
+  //   return possibleSchedules;
+  // }
 
   /// Get a list of all courses starting at or before a specified time
   List<Course> coursesAtOrBefore(DateTime time) => courses.sublist(
