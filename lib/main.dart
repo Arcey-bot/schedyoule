77b7cef530a36b2f9c3dc8517602512b59dd642d
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-07-11 04:14:57 -0400

    Update TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:schedyoule/views/home_view.dart';
 
 // TODO: Tips screen or something idk
-// TODO: Perhaps show card times in 24hr format
 // TODO: Opened/Closed schedules do not maintain state when offscreen
 // TODO: Sort generated schedules by credit count
 // TODO: Schedules get more fucked up the further down you scroll (Possibly fixed?)
