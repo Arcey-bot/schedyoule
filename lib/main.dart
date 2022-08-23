3d4e22ecc6bf6ffa217af86354da498801bfdfa6
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-08-23 19:15:23 -0400

    Remove TODO

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:schedyoule/views/home_view.dart';
 
 // TODO: Tips screen or something idk
-// TODO: Opened/Closed schedules do not maintain state when offscreen
 // TODO: Schedules get more fucked up the further down you scroll (Possibly fixed?)
 // TODO: Update animation (Entire card is no longer being rebuilt)
 
