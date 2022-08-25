7c1f2a7650cf733ac6143fb685b1d59fbc9f3705
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-08-25 01:15:32 -0400

    Implement course loading from SharedPreferences

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:schedyoule/constants/constants.dart';
+import 'package:schedyoule/data/models/course.dart';
+import 'package:schedyoule/providers/providers.dart';
 import 'package:schedyoule/views/course_list_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends ConsumerWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Load any preexisting courses before rendering the CourseListView
+    SharedPreferences.getInstance().then((instance) =>
+        ref.watch(savedCoursesProvider.notifier).state = instance
+            .getStringList(savedCourseKey)
+            ?.map((e) => Course.fromJson(e))
+            .toList());
+
     return const CourseListView();
   }
 }
