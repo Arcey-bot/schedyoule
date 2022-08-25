c88ec26a97e32d97e050ca6d313ed3a6e9764aab
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-08-25 01:14:59 -0400

    Ensure flutterbinding initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:schedyoule/views/home_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // TODO: Tips screen or something idk
 // TODO: Schedules get more fucked up the further down you scroll (Possibly fixed?)
 // TODO: Save courses on exit
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: MyApp()));
 }
 
