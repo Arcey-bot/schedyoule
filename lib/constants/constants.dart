551c784842089d6d1dae0054303b67b2e2d60a0b
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-08-25 23:45:48 -0400

    Add default course list and key for Hive storage

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -40,7 +40,7 @@ const double generateAnimationOpacity = 0.4; // Opacity [0, 1]
 const double generateAnimationOffset = 0.3; // [0, 1]
 const double generateAnimationSize = 0.25; // [0, 1]
 const Duration bubbleAnimationDuration = Duration(milliseconds: 250);
-const String savedCourseKey = 'courses';
+const String coursesBoxKey = 'courses';
 
 const List<String> courseCardPlaceholderTitles = [
   'Math',
@@ -50,4 +50,36 @@ const List<String> courseCardPlaceholderTitles = [
   'Chemistry',
   'English',
   'Spanish',
+];
+
+final List<Course> defaultCourseList = [
+  Course(
+    name: 'Physics',
+    time: TimeSlot.fromInt(
+        startHour: 10, startMinute: 0, endHour: 10, endMinute: 50),
+    days: {
+      DateTime.monday,
+      DateTime.wednesday,
+      DateTime.friday,
+    },
+    credits: 4,
+  ),
+  Course(
+    name: 'English',
+    time: TimeSlot.fromInt(
+        startHour: 9, startMinute: 0, endHour: 9, endMinute: 50),
+    days: {
+      DateTime.tuesday,
+      DateTime.thursday,
+      DateTime.friday,
+    },
+  ),
+  Course(
+    name: 'Math',
+    time: TimeSlot.fromInt(
+        startHour: 11, startMinute: 0, endHour: 12, endMinute: 50),
+    days: {
+      DateTime.thursday,
+    },
+  ),
 ];
