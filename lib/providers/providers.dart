919f7cff757ab49ad3dc52891d557b07d0ca1808
Vinny <44277279+Arcey-bot@users.noreply.github.com>
2022-08-25 23:47:31 -0400

    Transition to Hive to load locally-stored courses initially into provider

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,73 +1,28 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
 import 'package:schedyoule/constants/constants.dart';
 import 'package:schedyoule/data/models/models.dart';
 import 'package:schedyoule/data/repositories/course_schedule_repository.dart';
 import 'package:schedyoule/viewmodels/course_list_viewmodel.dart';
 
-final savedCoursesProvider = StateProvider<List<Course>?>(
-  (ref) => [],
-);
-
 final courseScheduleProvider =
     StateNotifierProvider<CourseListViewModel, CourseScheduleRepository>(
   (ref) {
-    return CourseListViewModel(ref.watch(savedCoursesProvider));
+    return CourseListViewModel(_readSavedCourses());
   },
 );
 
 final generateStateProvider =
     StateProvider<GenerateButtonState>((ref) => GenerateButtonState.ready);
 
-final courses = [
-  Course(
-    key: UniqueKey(),
-    name: 'Physics 2',
-    time: TimeSlot.fromInt(
-        startHour: 10, startMinute: 0, endHour: 10, endMinute: 50),
-    days: {
-      DateTime.monday,
-      DateTime.wednesday,
-      DateTime.friday,
-    },
-    credits: 4,
-  ),
-  Course(
-    key: UniqueKey(),
-    name: 'Test 2',
-    time: TimeSlot.fromInt(
-        startHour: 9, startMinute: 0, endHour: 9, endMinute: 50),
-    days: {
-      DateTime.tuesday,
-      DateTime.thursday,
-      DateTime.friday,
-    },
-  ),
-  Course(
-    key: UniqueKey(),
-    name: 'Math',
-    time: TimeSlot.fromInt(
-        startHour: 2, startMinute: 0, endHour: 2, endMinute: 50),
-    days: {
-      DateTime.thursday,
-    },
-  ),
-  Course(
-    key: UniqueKey(),
-    name: 'Sci',
-    time: TimeSlot.fromInt(
-        startHour: 11, startMinute: 0, endHour: 12, endMinute: 50),
-    days: {
-      DateTime.thursday,
-    },
-  ),
-  Course(
-    key: UniqueKey(),
-    name: 'Bio',
-    time: TimeSlot.fromInt(
-        startHour: 8, startMinute: 0, endHour: 8, endMinute: 50),
-    days: {
-      DateTime.thursday,
-    },
-  ),
-];
+/// Returns a list of every course saved in local storage or empty list if none
+List<Course> _readSavedCourses() {
+  final List<Course> loadedCourses = [];
+
+  // Loads every saved course one at a time
+  for (int i = 0; i < Hive.box(coursesBoxKey).length; i++) {
+    loadedCourses.add(Course.fromJson(Hive.box(coursesBoxKey).getAt(i)));
+  }
+
+  return loadedCourses.isNotEmpty ? loadedCourses : defaultCourseList;
+}
